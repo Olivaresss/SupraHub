41ea1cd4f90518a7d77ec9a6ed51d29522ac5c92
Olivaresss <rodrigo.olivares@sptech.school>
2024-11-23 22:43:43 -0300

    Atualizando back-end login e cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -25,7 +25,6 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         username: resultadoAutenticar[0],
                                         senha: resultadoAutenticar[0].senha,
-                                        fkCar: resultadoAutenticar[0].fkCar
                                     });
                             
                            
@@ -48,24 +47,21 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var usuario = req.body.usuarioServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (usuario == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(usuario, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
